8eabbad687b978ab617253353c5d43d9f181cc13
healimfx <22000770@handong.edu>
2021-08-02 02:55:55 +0900

    Create 5-7_최혜림_20210731.cpp

---
+++ week4/최혜림_20210731/5-7_최혜림_20210731.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    string cc[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+    cin >> str;
+    
+    for (int i=0; i<str.size(); i++) {
+        string a = str[i] + str[i+1];
+        string b = str[i] + str[i+1] + str[i+2];
+
+        for (int j=0; j<)
+    }
+}

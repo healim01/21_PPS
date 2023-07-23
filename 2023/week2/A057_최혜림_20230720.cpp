cd518bcfe5b818ec7ec5223914cc69ab5b8d15d2
healim01 <22000770@handong.edu>
2023-07-23 23:50:05 +0900

    week2

---
+++ 2023/week2/A057_최혜림_20230720.cpp
@@ -0,0 +1,20 @@
+// 2021 07 10
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pay, make, price;
+    int sell = 0;
+    cin >> pay >> make >> price;
+
+    int add = price - make;
+
+    if (add <= 0) {
+        cout << -1 << endl;
+    } 
+    else {
+        sell = pay / add + 1;
+        cout << sell << endl;
+    }
+    return 0;
+}
